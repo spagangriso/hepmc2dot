3fa48e05bd694b015646f54a65e49e6b8c5f5a2b
Elmar Ritsch <elritsch@users.noreply.github.com>
2016-11-03 14:09:14 +0100

    Add particle PDG id and energy to dot output + Refactor code

--- hepmc2dot.py
+++ hepmc2dot.py
@@ -5,23 +5,64 @@ import re # regex
 import numpy as np
 
 
-def _get_dot_vertex(vtx_id, r, z, is_dummy=False):
+def _get_dot_particle(prod_vtx_barcode, end_vtx_barcode,
+                      particle_barcode, particle_id, particle_energy):
     """
-    Generates a DOT formatted vertex for the given id and coordinates
+    Returns a string containing a DOT formatted edge which represents a particle travelling from
+    the given production to the given end vertex. If end_vtx_barcode is None, the edge will connect
+    to a dummy end vertex.
+    """
+    prod_vtx = _get_node_name(prod_vtx_barcode)
+    if not end_vtx_barcode:
+        end_vtx = _get_node_name(particle_barcode, is_dummy=True)
+    else:
+        end_vtx = _get_node_name(end_vtx_barcode)
+
+    particle_dot = '    {prod_vtx} -> {end_vtx} [label="p #{bc}\\n' \
+                   'id={part_id}\\n' \
+                   'E={energy:.0f}"];\n'.format(prod_vtx=prod_vtx,
+                                                end_vtx=end_vtx,
+                                                bc=particle_barcode,
+                                                part_id=particle_id,
+                                                energy=float(particle_energy))
+    return particle_dot
+
+
+def _get_node_name(barcode, is_dummy=False):
+    """
+    Returns the DOT node name for the given barcode
+
+    DOT nodes represent either an interaction vertex or a dummy end vertex for a final state
+    particle
     """
     if is_dummy:
         dummy = 'dummy_'
-        attrib = 'shape=none,label=""'
     else:
         dummy = ''
-        attrib = r'label="vtx {ID}\nr={r:.2f},z={z:.2f}"'.format(ID=vtx_id, r=r, z=z)
 
-    dot = '    V_{dummy}{abs_id} [{attrib},pos="{zpos:.3f},{rpos:.3f}!"];\n'.format(
-                                                                                dummy=dummy,
-                                                                                abs_id=abs(int(vtx_id)),
-                                                                                attrib=attrib,
-                                                                                zpos=z,
-                                                                                rpos=r)
+    abs_bc = abs(int(barcode))
+    node_name = 'V_{dummy}{abs_bc}'.format(dummy=dummy, abs_bc=abs_bc)
+    return node_name
+
+
+def _get_dot_vertex(barcode, r, z, is_dummy=False, scale=1.):
+    """
+    Generates a DOT formatted string representing an interaction (or dummy) vertex for the given
+    barcode and coordinates
+    """
+    barcode = int(barcode)
+    r = float(r)
+    z = float(z)
+    if is_dummy:
+        attrib = 'shape=none,label=""'
+    else:
+        attrib = r'label="vtx #{bc}\nr={r:.2f},z={z:.2f}"'.format(bc=barcode, r=r, z=z)
+
+    vtx_name = _get_node_name(barcode, is_dummy)
+    dot = '    {node} [{attrib},pos="{zpos:.3f},{rpos:.3f}!"];\n'.format(node=vtx_name,
+                                                                         attrib=attrib,
+                                                                         zpos=z*scale,
+                                                                         rpos=r*scale)
     return dot
 
 
@@ -34,12 +75,12 @@ class HepDotWriter(object):
         self.dotfile = open(dotfile, 'w')
 
         self.event_open = False
-        self.cur_vtx = None
+        self.cur_vtx_barcode = None
         self.cur_vtx_r = None
         self.cur_vtx_z = None
 
         # TODO: implement next:
-        #self.vtx_threshold = vtx_threshold
+        #self.vtx_threshold = np.nan # vtx_threshold
         self.scale = 1. #scale
         # primary only:
         #self.vtx_threshold = 200000
@@ -54,12 +95,12 @@ class HepDotWriter(object):
 
     def newVertex(self, raw_hepmc_line):
         hepmc = raw_hepmc_line.split()
-        vtx_id_column = 1
-        vtx_id = int(hepmc[vtx_id_column])
-        vtx_abs_id = abs(vtx_id)
+        vtx_barcode_column = 1
+        vtx_barcode = int(hepmc[vtx_barcode_column])
+        vtx_abs_barcode = abs(vtx_barcode)
 
         # TODO: implement next
-        #if vtx_abs_id > self.vtx_threshold:
+        #if vtx_abs_barcode > self.vtx_threshold:
         #    return
 
         x_column = 3
@@ -71,17 +112,24 @@ class HepDotWriter(object):
         self.cur_vtx_z = float(hepmc[z_column])
         self.cur_vtx_r = math.sqrt(x**2 + y**2)
 
-        self.cur_vtx = 'V_{abs_id}'.format(abs_id=vtx_abs_id)
-        dot_vtx = _get_dot_vertex(vtx_id,
-                                  self.cur_vtx_r*self.scale,
-                                  self.cur_vtx_z*self.scale)
+        self.cur_vtx_barcode = vtx_barcode
+        dot_vtx = _get_dot_vertex(vtx_barcode,
+                                  self.cur_vtx_r,
+                                  self.cur_vtx_z,
+                                  scale=self.scale)
         self.dotfile.write(dot_vtx)
 
     def addOutgoingParticle(self, raw_hepmc_line):
         line = raw_hepmc_line.split()
 
-        particle_num_column = 1
-        particle_num = line[particle_num_column]
+        particle_barcode_column = 1
+        particle_barcode = line[particle_barcode_column]
+
+        particle_id_column = 2
+        particle_id = line[particle_id_column]
+
+        particle_energy_column = 6
+        particle_energy = float(line[particle_energy_column])
 
         mom_x_column = 3
         mom_x = float(line[mom_x_column])
@@ -90,14 +138,14 @@ class HepDotWriter(object):
         mom_z_column = 5
         mom_z = float(line[mom_z_column])
 
-        end_vtx_num_column = 11
-        end_vtx_num = abs(int(line[end_vtx_num_column]))
+        end_vtx_barcode_column = 11
+        end_vtx_barcode = abs(int(line[end_vtx_barcode_column]))
 
         # TODO: implement next
-        #if end_vtx_num > self.vtx_threshold:
+        #if end_vtx_barcode > self.vtx_threshold:
         #    return
 
-        if not end_vtx_num:
+        if not end_vtx_barcode:
             # create dummy end node for partiles that don't have end vertices
 
             mom_r = math.sqrt(mom_x**2 + mom_y**2)
@@ -107,17 +155,14 @@ class HepDotWriter(object):
             end_vtx_r = self.cur_vtx_r*self.scale + mom_r/mom_abs*particle_len
             end_vtx_z = self.cur_vtx_z*self.scale + mom_z/mom_abs*particle_len
 
-            dot_vtx = _get_dot_vertex(particle_num, end_vtx_r, end_vtx_z, is_dummy=True)
+            dot_vtx = _get_dot_vertex(particle_barcode, end_vtx_r, end_vtx_z, is_dummy=True)
             self.dotfile.write(dot_vtx)
 
-            end_vtx = 'V_dummy_{num}'.format(num=particle_num)
-        else:
-            end_vtx = 'V_{num}'.format(num=end_vtx_num)
-
-        particle_dot = '    {cur_vtx} -> {end_vtx} [label="p {num}"];\n'.format(
-                                                                            cur_vtx=self.cur_vtx,
-                                                                            end_vtx=end_vtx,
-                                                                            num=particle_num)
+        particle_dot = _get_dot_particle(self.cur_vtx_barcode,
+                                         end_vtx_barcode,
+                                         particle_barcode,
+                                         particle_id,
+                                         particle_energy)
         self.dotfile.write(particle_dot)
 
     def close(self):
--- test/test_hepmc2dot.py
+++ test/test_hepmc2dot.py
@@ -4,6 +4,60 @@ import unittest
 
 import os
 import tempfile
+from math import sqrt
+
+
+class Test_get_dot_particle(unittest.TestCase):
+
+    def test_floatEnergy_expectEnergyRounding(self):
+        actual_dot_particle = hepmc2dot._get_dot_particle(prod_vtx_barcode=123,
+                                                          end_vtx_barcode=987,
+                                                          particle_barcode=666,
+                                                          particle_id=-111,
+                                                          particle_energy=999.9)
+        expected_dot_particle = '    V_123 -> V_987 [label="p #666\\nid=-111\\nE=1000"];\n'
+        self.assertEqual(expected_dot_particle, actual_dot_particle)
+
+    def test_finalStateParticle_expectDummyEndVertex(self):
+        actual_dot_particle = hepmc2dot._get_dot_particle(prod_vtx_barcode=123,
+                                                          end_vtx_barcode=None,
+                                                          particle_barcode=666,
+                                                          particle_id=-111,
+                                                          particle_energy=888)
+        expected_dot_particle = '    V_123 -> V_dummy_666 [label="p #666\\nid=-111\\nE=888"];\n'
+        self.assertEqual(expected_dot_particle, actual_dot_particle)
+
+    def test_stringArguments_expectAutomaticConversion(self):
+        actual_dot_particle = hepmc2dot._get_dot_particle(prod_vtx_barcode='123',
+                                                          end_vtx_barcode='987',
+                                                          particle_barcode='666',
+                                                          particle_id='-111',
+                                                          particle_energy='888')
+        expected_dot_particle = '    V_123 -> V_987 [label="p #666\\nid=-111\\nE=888"];\n'
+        self.assertEqual(expected_dot_particle, actual_dot_particle)
+
+
+class Test_get_node_name(unittest.TestCase):
+
+    def test_positiveBarcode_expectPositiveNumInNodeName(self):
+        actual_node_name = hepmc2dot._get_node_name(barcode=123)
+        expected_node_name = 'V_123'
+        self.assertEqual(actual_node_name, expected_node_name)
+
+    def test_negativeBarcode_expectPositiveNumInNodeName(self):
+        actual_node_name = hepmc2dot._get_node_name(barcode=-123)
+        expected_node_name = 'V_123'
+        self.assertEqual(actual_node_name, expected_node_name)
+
+    def test_dummyVertex_expectDummyNodeName(self):
+        actual_node_name = hepmc2dot._get_node_name(barcode=-123, is_dummy=True)
+        expected_node_name = 'V_dummy_123'
+        self.assertEqual(actual_node_name, expected_node_name)
+
+    def test_strArguments_expectAutomaticConversion(self):
+        actual_node_name = hepmc2dot._get_node_name(barcode='-123', is_dummy=True)
+        expected_node_name = 'V_dummy_123'
+        self.assertEqual(actual_node_name, expected_node_name)
 
 
 class Test_get_dot_vertex(unittest.TestCase):
@@ -16,14 +70,14 @@ class Test_get_dot_vertex(unittest.TestCase):
         vtx_id = -1
         vtx_r = 2
         vtx_z = 3
-        expected_dot = '    V_1 [label="vtx -1\\nr=2.00,z=3.00",pos="3.000,2.000!"];\n'
+        expected_dot = '    V_1 [label="vtx #-1\\nr=2.00,z=3.00",pos="3.000,2.000!"];\n'
         self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z)
 
     def test_positiveVertexID_expectPositiveDotVertexNameAndPositiveIDInLabel(self):
         vtx_id = 1
         vtx_r = 2
         vtx_z = 3
-        expected_dot = '    V_1 [label="vtx 1\\nr=2.00,z=3.00",pos="3.000,2.000!"];\n'
+        expected_dot = '    V_1 [label="vtx #1\\nr=2.00,z=3.00",pos="3.000,2.000!"];\n'
         self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z)
 
     def test_dummyVertex_expectDummyVertexNameAndEmptyLabel(self):
@@ -46,18 +100,56 @@ class Test_get_dot_vertex(unittest.TestCase):
         vtx_id = -1
         vtx_r = 2.3456789
         vtx_z = 3.4567890
-        expected_dot = '    V_1 [label="vtx -1\\nr=2.35,z=3.46",pos="3.457,2.346!"];\n'
+        expected_dot = '    V_1 [label="vtx #-1\\nr=2.35,z=3.46",pos="3.457,2.346!"];\n'
         self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z)
 
+    def test_strVtxArguments_expectAutomaticConversion(self):
+        vtx_id = '-1'
+        vtx_r = '2.3456789'
+        vtx_z = '3.4567890'
+        expected_dot = '    V_1 [label="vtx #-1\\nr=2.35,z=3.46",pos="3.457,2.346!"];\n'
+        self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z)
+
+    def test_scaleProvided_expectScaledPosition(self):
+        vtx_id = -1
+        vtx_r = 2
+        vtx_z = 3
+        scale = 4
+
+        expected_dot = '    V_1 [label="vtx #-1\\nr=2.00,z=3.00",pos="12.000,8.000!"];\n'
+        self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z, scale=scale)
+
+    def test_scaledDummy_expectScaledPosition(self):
+        vtx_id = -1
+        vtx_r = 2
+        vtx_z = 3
+        is_dummy = True
+        scale = 4
+
+        expected_dot = '    V_dummy_1 [shape=none,label="",pos="12.000,8.000!"];\n'
+        self.assert_get_vertex(expected_dot, vtx_id, vtx_r, vtx_z, is_dummy=is_dummy, scale=scale)
 
 
-class TestHepMC2DotComponent(unittest.TestCase):
+class TestComponentHepMC2Dot(unittest.TestCase):
 
     def setUp(self):
         self.hepmc_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
 
         self.dot_file = tempfile.NamedTemporaryFile(delete=False, mode='r')
         self.dot_file.close()
+
+        # globally define particle and vertex DOT strings for test maintainability
+        self.vtx_200334 = '    V_200334 [label="vtx #-200334\\nr=1027.68,z=1423.66",pos="1423.657,1027.677!"];\n'
+        self.vtx_200648 = '    V_200648 [label="vtx #-200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n'
+        self.vtx_dummy_200386 = '    V_dummy_200386 [shape=none,label="",pos="-1943.048,1281.427!"];\n'
+        self.vtx_dummy_200389 = '    V_dummy_200389 [shape=none,label="",pos="-1740.250,1232.510!"];\n'
+        self.vtx_dummy_200391 = '    V_dummy_200391 [shape=none,label="",pos="-1913.914,1288.463!"];\n'
+        self.vtx_dummy_200394 = '    V_dummy_200394 [shape=none,label="",pos="1548.247,1184.129!"];\n'
+        self.p_200388 = '    V_200648 -> V_200334 [label="p #200388\\nid=211\\nE=356"];\n'
+        self.p_200391 = '    V_200648 -> V_dummy_200391 [label="p #200391\\nid=2212\\nE=1042"];\n'
+        self.p_200386 = '    V_200648 -> V_dummy_200386 [label="p #200386\\nid=2112\\nE=1087"];\n'
+        self.p_200389 = '    V_200648 -> V_dummy_200389 [label="p #200389\\nid=-211\\nE=1077"];\n'
+        self.p_200394 = '    V_200334 -> V_dummy_200394 [label="p #200394\\nid=2112\\nE=1017"];\n'
 
     def tearDown(self):
         self.hepmc_file.close()
@@ -97,7 +189,7 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
+                                + self.vtx_200648 + \
                                 '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
 
@@ -112,10 +204,10 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
-                                '    V_dummy_200386 [shape=none,label="",pos="-1943.048,1281.427!"];\n' \
-                                '    V_200648 -> V_dummy_200386 [label="p 200386"];\n' \
-                                '}\n'
+                                + self.vtx_200648 \
+                                + self.vtx_dummy_200386 \
+                                + self.p_200386 \
+                                + '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
 
     def test_oneVertexTwoOutgoingParticlesWithoutEndVertices_expectSameRepresentationInDot(self):
@@ -130,13 +222,12 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
-                                '    V_dummy_200386 [shape=none,label="",pos="-1943.048,1281.427!"];\n' \
-                                '    V_200648 -> V_dummy_200386 [label="p 200386"];\n' \
-                                '    V_dummy_200391 [shape=none,label="",pos="-1913.914,1288.463!"];\n' \
-                                '    V_200648 -> V_dummy_200391 [label="p 200391"];\n' \
-                                '}\n'
-
+                                + self.vtx_200648 \
+                                + self.vtx_dummy_200386 \
+                                + self.p_200386 \
+                                + self.vtx_dummy_200391 \
+                                + self.p_200391 \
+                                + '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
 
     def test_eventTwoVerticesWithOneConnectingParticle_expectSameRepresentationInDot(self):
@@ -151,11 +242,10 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
-                                '    V_200648 -> V_200334 [label="p 200388"];\n' \
-                                '    V_200334 [label="vtx -200334\\nr=1027.68,z=1423.66",pos="1423.657,1027.677!"];\n' \
-                                '}\n'
-
+                                + self.vtx_200648 \
+                                + self.p_200388 \
+                                + self.vtx_200334 \
+                                + '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
 
     def test_eventTwoVerticesWithOneConnectingParticleAndOneParticleWithoutEndVertex_expectSameRepresentationInDot(self):
@@ -171,12 +261,12 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
-                                '    V_200648 -> V_200334 [label="p 200388"];\n' \
-                                '    V_dummy_200389 [shape=none,label="",pos="-1740.250,1232.510!"];\n' \
-                                '    V_200648 -> V_dummy_200389 [label="p 200389"];\n' \
-                                '    V_200334 [label="vtx -200334\\nr=1027.68,z=1423.66",pos="1423.657,1027.677!"];\n' \
-                                '}\n'
+                                + self.vtx_200648 \
+                                + self.p_200388 \
+                                + self.vtx_dummy_200389 \
+                                + self.p_200389 \
+                                + self.vtx_200334 \
+                                + '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
 
     def test_eventTwoVerticesWithOneConnectingParticleAndTwoParticlesWithoutEndVertices_expectSameRepresentationInDot(self):
@@ -193,12 +283,12 @@ class TestHepMC2DotComponent(unittest.TestCase):
         with open(self.dot_file.name, 'r') as result_file:
             actual_dot_contents = result_file.read()
         expected_dot_contents = 'digraph event_29 {\n' \
-                                '    V_200648 [label="vtx -200648\\nr=1091.08,z=-1881.66",pos="-1881.663,1091.080!"];\n' \
-                                '    V_200648 -> V_200334 [label="p 200388"];\n' \
-                                '    V_dummy_200389 [shape=none,label="",pos="-1740.250,1232.510!"];\n' \
-                                '    V_200648 -> V_dummy_200389 [label="p 200389"];\n' \
-                                '    V_200334 [label="vtx -200334\\nr=1027.68,z=1423.66",pos="1423.657,1027.677!"];\n' \
-                                '    V_dummy_200394 [shape=none,label="",pos="1548.247,1184.129!"];\n' \
-                                '    V_200334 -> V_dummy_200394 [label="p 200394"];\n' \
-                                '}\n'
+                                + self.vtx_200648 \
+                                + self.p_200388 \
+                                + self.vtx_dummy_200389 \
+                                + self.p_200389 \
+                                + self.vtx_200334 \
+                                + self.vtx_dummy_200394 \
+                                + self.p_200394 \
+                                + '}\n'
         self.assertEqual(expected_dot_contents, actual_dot_contents)
